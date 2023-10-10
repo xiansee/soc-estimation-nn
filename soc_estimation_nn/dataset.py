dfa10811addbc386db38116b9a61e9cdc39573fe
Xian <xian.see@outlook.com>
2023-10-09 20:55:19 -0400

    Added DataModule, moved normalization to Dataset level

--- soc_estimation_nn/dataset.py
+++ soc_estimation_nn/dataset.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import os
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch import Tensor
+import lightning.pytorch as pl
 
 class BatterySOCDataset(Dataset):
 
     time_col = 'Time [min]'
-    voltage_col = 'Normalized Voltage [-]'
-    current_col = 'Normalized Current [-]'
-    temperature_col = 'Normalized Temperature [-]'
+    voltage_col = 'Voltage [V]'
+    current_col = 'Current [A]'
+    temperature_col = 'Temperature [degC]'
+    capacity_col = 'Capacity [Ah]'
     soc_col = 'SOC [-]'
 
+    norm_settings = {
+        voltage_col: (2.5, 4.2),
+        current_col: (-20, 20),
+        temperature_col: (-30, 50),
+    }
 
     def __init__(self, data_directory: str):
         self.data = []
@@ -24,6 +32,7 @@ class BatterySOCDataset(Dataset):
             ))
             self.data.extend([pd.read_parquet(f'{T_directory}/{f}') for f in file_names])
             self.dataset_names.extend(file_names)
+            break
 
 
     def get_dataset_name(self, index: int):
@@ -36,6 +45,14 @@ class BatterySOCDataset(Dataset):
 
         t = torch.tensor(df[self.time_col], dtype=torch.float32).view(data_length, 1)
         return t
+    
+
+    def normalize_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        for col, norm_range in self.norm_settings.items():
+            min_val, max_val = norm_range
+            df[col] = (df[col] - min_val) / (max_val - min_val) * 2 - 1
+
+        return df
 
 
     def __len__(self):
@@ -44,6 +61,7 @@ class BatterySOCDataset(Dataset):
 
     def __getitem__(self, index: int):
         df = self.data[index]
+        df = self.normalize_data(df)
 
         X_cols = [self.voltage_col, self.current_col, self.temperature_col]
         Y_col = self.soc_col
@@ -53,3 +71,48 @@ class BatterySOCDataset(Dataset):
         Y = torch.tensor(df[Y_col], dtype=torch.float32).view(data_length, 1)
 
         return X, Y
+
+
+class BatterySOCDataModule(pl.LightningDataModule):
+
+
+    def __init__(
+            self, 
+            data_directory: str, 
+            train_split: float = 0.7, 
+            val_split: float = 0.2,
+            test_split: float = 0.1
+        ):
+        super().__init__()
+        self.data_directory = data_directory
+
+        if round(sum([train_split, val_split, test_split]), 6) != 1:
+            raise ValueError('All of train/val/test splits must sum up to 1.')
+        
+        self.train_split = train_split
+        self.val_split = val_split
+        self.test_split = test_split
+
+
+    def setup(self, stage: str):
+        dataset = BatterySOCDataset(self.data_directory)
+        num_dataset = len(dataset)
+        num_training_set = round(self.train_split * num_dataset)
+        num_validation_set = round(self.val_split * num_dataset)
+        num_test_set = round(self.test_split * num_dataset)
+
+        self.training_set, self.validation_set, self.test_set = random_split(
+            dataset, [num_training_set, num_validation_set, num_test_set]
+        )
+
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.training_set, batch_size=1, shuffle=True)
+    
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.validation_set, batch_size=1, shuffle=True)
+    
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.test_set, batch_size=1, shuffle=True)
